f582c02664318ea78a577624196d71f3cea7381b
Joel Gorin <joelmgr@gmail.com>
2016-11-25 13:50:11 -0600

    Add files via upload

--- DB_Connect.cs
+++ DB_Connect.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Progra_3
 {
     class DB_Connect
     {
+
+        static int Main(string[] args)
+        {
+            DB_Connect test = new DB_Connect();
+            test.OpenConnection();
+            test.Insert();
+            test.CloseConnection();
+            return 0;
+        }
+
         private MySqlConnection connection;
         private string server;
         private string database;
@@ -17,16 +29,25 @@ namespace Progra_3
         //Constructor
         public DB_Connect()
         {
+            server = "localhost";
+            database = "expediente_medico";
+            uid = "root";
+            password = "DataBase2016";
+            Initialize();
+        }
+
+        public DB_Connect(string server, string database, string uid, string password)
+        {
+            this.server = server;
+            this.database = database;
+            this.uid = uid;
+            this.password = password;
             Initialize();
         }
 
         //Initialize values
         private void Initialize()
         {
-            server = "localhost";
-            database = "connectcsharptomysql";
-            uid = "username";
-            password = "password";
             string connectionString;
             connectionString = "SERVER=" + server + ";" + "DATABASE=" +
             database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
@@ -37,16 +58,64 @@ namespace Progra_3
         //open connection to database
         private bool OpenConnection()
         {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
         }
 
         //Close connection
         private bool CloseConnection()
         {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
         }
 
-        //Insert statement
+        //Insert statement string tableName, string[] columns, string[] values
         public void Insert()
         {
+            string query = "INSERT INTO Telefonos (telefono, cedula) VALUES(88888888, 188888888)";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
         }
 
         //Update statement
@@ -62,11 +131,69 @@ namespace Progra_3
         //Select statement
         public List<string>[] Select()
         {
+            string query = "SELECT * FROM tableinfo";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["id"] + "");
+                    list[1].Add(dataReader["name"] + "");
+                    list[2].Add(dataReader["age"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
         }
 
         //Count statement
         public int Count()
         {
+            string query = "SELECT Count(*) FROM tableinfo";
+            int Count = -1;
+
+            //Open Connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
         }
 
         //Backup
@@ -79,4 +206,6 @@ namespace Progra_3
         {
         }
     }
+
+
 }
